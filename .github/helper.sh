a5311b0352bc625a028783b23675b758c258a2fd
Pietje Puk <pietje@pietjepuk.net>
2025-11-09 22:58:58 +0100

    fix spaces in env?

--- .github/helper.sh
+++ .github/helper.sh
@@ -3,10 +3,13 @@
 set_value_in_env() {
     # Assumes that "var" has been set to the name of the variable to be changed
     if [ "${!var+x}" ]; then
+        # Quote the value to handle spaces and special characters
+        local quoted_val
+        quoted_val=$(printf '%q' "${!var}")
         if ! grep -qF "${var}" .env 2>/dev/null ; then
-            echo "${var}=${!var}" >> .env
+            echo "${var}=${quoted_val}" >> .env
         else
-            sed -i'.original' -e "s~^\(${var}\s*=\s*\).*$~\1${!var}~" .env
+            sed -i'.original' -e "s~^\(${var}\s*=\s*\).*$~\1${quoted_val}~" .env
         fi
     fi
 }
